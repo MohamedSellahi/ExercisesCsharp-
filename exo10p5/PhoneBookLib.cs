de4aa98b7d48f72654bddc9569f91ecbc4254a0f
MohamedSellahi <mohamed.sellahi@gmail.com>
2017-02-23 00:16:25 +0100

    ajout méthode impression contact

---
+++ exo10p5/PhoneBookLib.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PhoneBookLib {
+  class ManipulatePhoneBook {
+
+    public static void SayHi() {
+      Console.WriteLine("Hi from lib");
+    }
+
+
+  }
+}
--- exo10p5/exo10p5/Program.cs
+++ exo10p5/exo10p5/Program.cs
@@ -5,17 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
+
 namespace exo10p5 {
   class Program {
     static void Main(string[] args) {
 
-      string filePath = @"C:\Users\34011-36-05\Documents\Visual Studio 2015\Projects\exo10p5\repertoire.txt";
+      
+      //string filePath = @"C:\Users\34011-36-05\Documents\Visual Studio 2015\Projects\exo10p5\repertoire.txt";
+      string filePath = @"C:\Users\Mohamed\Documents\Visual Studio 2015\Projects\ExercisesCsharp-\exo10p5\repertoire.txt"; 
       ContactProperties props;
       props.maxNameLen = 16;
       props.maxTelLen = 13;
       props.maxMailLen = 30;
 
-      string nom, tel, mail, UserInput;
+      string UserInput;
       char delimiter = ' ';
       int nContacts = 0;
 
@@ -28,13 +31,25 @@ namespace exo10p5 {
 
         while((tmp = sr.ReadLine())!=null) { // read contacts: we suppose constant width fields 
                                              // TODO discard empty lines 
-          Contact entry;
-          entry.Nom = tmp.Substring(0, props.maxNameLen);
-          entry.NumTel = tmp.Substring(props.maxNameLen, props.maxTelLen);
-          entry.Mail = tmp.Substring(props.maxNameLen + props.maxTelLen);
 
-          Carnet.Add(entry);
-          ++nContacts;  // maybe not necessary 
+          Contact entry;
+          if(string.IsNullOrWhiteSpace(tmp)) { // discard white spaces
+            Console.WriteLine("Contact vide détecté");
+          }
+          else if(tmp.Length != props.maxMailLen + 
+                                props.maxNameLen +
+                                props.maxTelLen) {// contact not in correct format
+            Console.WriteLine("format entry corrumpu");// TODO: proposer correction
+          }
+          else {
+            entry.Nom = tmp.Substring(0, props.maxNameLen);
+            entry.NumTel = tmp.Substring(props.maxNameLen, props.maxTelLen);
+            entry.Mail = tmp.Substring(props.maxNameLen + props.maxTelLen);
+
+            Carnet.Add(entry);
+            ++nContacts;  // maybe not necessary 
+          }
+        
         }
 
       }
@@ -46,9 +61,12 @@ namespace exo10p5 {
         //Console.Clear();
         Console.WriteLine("Veuiller entrer une operation: ");
         Console.WriteLine("a- Ajouter contact\n"+
-                          "e- Editer contact \n"+
+                          "e- Editer contact\n"+
                           "s- Supprimer contact\n"+
-                          "q- Quitter          \n");
+                          "p- Afficher info contact\n"+
+                          "d- Afficher tous les contacts\n"+
+                          "c- Clear\n"+
+                          "q- Quitter\n");
 
         UserInput = Console.ReadLine();  // TODO try catch ? 
         switch(UserInput) {
@@ -64,10 +82,22 @@ namespace exo10p5 {
           case "S":
             DeleteContacts(Carnet, props);
             break;
+          case "p":
+          case "P":
+            PrintContactsWrapper(Carnet, props);
+            break;
+          case "d":
+          case "D":
+            PrintAllContacts(Carnet);
+            break; 
           case "q":
           case "Q":
             // quit 
             Ongoing = false;
+            break;
+          case "c":
+          case "C":
+            Console.Clear();
             break;
             
           default:
@@ -214,7 +244,8 @@ namespace exo10p5 {
       // function to promp the user for edit contact  
       public static void EditContacts(List<Contact> carnet, ContactProperties props) {
       Console.WriteLine("Quelle est le nom du contact à editer: ");
-      string tmpName = Console.ReadLine(); // todo try catch 
+      string tmpName = Console.ReadLine(); // todo try catch
+      tmpName = TrimInput(ref tmpName, props.maxNameLen); 
       int index = ContactExists(carnet, tmpName);
 
       if(index > -1) { // contact exists proceed to edit 
@@ -223,9 +254,8 @@ namespace exo10p5 {
       else {
         Console.WriteLine("Contact n'exist pas: voulez vous l'ajouter (O/N): ");
         string choice = Console.ReadLine();
-        if(choice == "O") { // proceed to add 
-
-
+        if(choice == "O") { // proceed to add
+          AddContact(carnet, tmpName, props);
         }
         else {
           return; 
@@ -233,22 +263,32 @@ namespace exo10p5 {
 
       }
 
-
-
     }
 
     public static void EditContact(List<Contact> carnet,string name, int index, ContactProperties props) {
+      PrintContactInfo(carnet[index]);
       Contact tmpContact;
       string userInput;
 
       tmpContact.Nom = name;  
-      Console.WriteLine("Entrer le numéro de téléphone: ");
-      userInput = Console.ReadLine(); // TODO try catch 
-      tmpContact.NumTel = TrimInput(ref userInput,props.maxTelLen) ;
-
-      Console.WriteLine("Entrer l'émail: "); // TODO try catch 
+      Console.WriteLine("Entrer le numéro de téléphone (p pour passer): ");
+      userInput = Console.ReadLine(); // TODO try catch
+      if(userInput!= "p") {
+        tmpContact.NumTel = TrimInput(ref userInput, props.maxTelLen);
+      }
+      else { // keep it as it is 
+        tmpContact.NumTel = carnet[index].NumTel; 
+      }
+      
+      Console.WriteLine("Entrer l'émail (p pour passer): "); // TODO try catch 
       userInput = Console.ReadLine();
-      tmpContact.Mail = TrimInput(ref userInput, props.maxMailLen);
+      if(userInput != "p") {
+        tmpContact.Mail = TrimInput(ref userInput, props.maxMailLen);
+      }
+      else {
+        tmpContact.Mail = carnet[index].Mail; 
+      }     
+        
       carnet[index] = tmpContact; 
     }
 
@@ -295,6 +335,62 @@ namespace exo10p5 {
       return str;
     }
 
+    // print contacts/ contact 
+    // printContact wrapper 
+
+      public static void PrintContactsWrapper(List<Contact> carnet, ContactProperties props) {
+
+      if(carnet.Count == 0) { // empty directory : 
+        PrintError("Carnet d'address vide");
+        return; 
+      }
+
+      Console.WriteLine("Entrer le nom du contact à afficher: ");
+      string tmpName = Console.ReadLine(); // TODO : try catch 
+      tmpName = TrimInput(ref tmpName, props.maxNameLen);
+      int index = ContactExists(carnet, tmpName);
+
+      if(index > -1) { // contact exists 
+        PrintContactInfo(carnet[index]);
+      }
+      else {
+        PrintError("Contact non trouvé"); 
+      }
+    }
+
+    // print all contacts 
+    public static void PrintAllContacts(List<Contact> carnet){
+      if(carnet.Count == 0) {
+        PrintError("Carnet d'address vide");
+        return; 
+      }
+
+      int pos = 0;
+      Console.WriteLine('\n');
+      Console.WriteLine("-----------------------------------------------------------+");
+      Console.WriteLine("Nom             Tel          Mail                          |");
+      Console.WriteLine("-----------------------------------------------------------+");
+      while(pos < carnet.Count) {
+        Console.WriteLine("{0}{1}{2}", carnet[pos].Nom
+                                     , carnet[pos].NumTel
+                                     , carnet[pos].Mail);
+        ++pos; 
+      }
+      Console.WriteLine();
+      Console.WriteLine("-----------------------------------------------------------");
+      Console.WriteLine();
+    }
+
+
+
+
+    public static void PrintContactInfo(Contact c) {
+      
+      Console.WriteLine("Nom             Tel          Mail");
+      Console.WriteLine("---------------------------------------------");
+      Console.WriteLine("{0}{1}{2}", c.Nom, c.NumTel, c.Mail);
+      Console.WriteLine("\n");
+    }
 
 
 
@@ -303,6 +399,8 @@ namespace exo10p5 {
     static public void PrintError(string message) {
       Console.WriteLine("Erreur: " + message);
     }
+
+    // print success message
     static public void PrintSuccess(string message) {
       Console.WriteLine(message);
     }
