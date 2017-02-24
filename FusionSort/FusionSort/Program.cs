15867745b601dc49b6fd84a2cac9204cad8fe6fc
MohamedSellahi <mohamed.sellahi@gmail.com>
2017-02-24 11:59:23 +0100

    vendredi

--- FusionSort/FusionSort/Program.cs
+++ FusionSort/FusionSort/Program.cs
@@ -11,18 +11,16 @@ namespace FusionSort {
       int[] tab2 = { -2, 2, 3, 8 };
 
       LinkedList<int> list = new LinkedList<int>(tab1);
-      LinkedList<int> Node;
-
-
-
-      while((Node = list.)) {
-
-      }
+      LinkedListNode<int> Node;
 
 
 
 
-      /*
+
+
+
+
+
       int[] res = Fusion(tab1, tab2);
       int[] res2 = PartialFusion(tab1, tab2, 0, tab1.Length - 1, 0, 1);
 
@@ -40,134 +38,133 @@ namespace FusionSort {
       int[] res3 = FusionSort(tab, 0, 5);
 
 
-      
-    }
-
-      */
-
-
-
-
-
-      /*
-
-          public static int[] FusionSort(int[] tab, int begin, int end) {
-            if(tab.Length <= 1) {
-              return tab;
-            }
-            else {
-
-              return PartialFusion(FusionSort(tab, begin, end / 2),
-                                   FusionSort(tab, end/2 + 1, end),
-                                   begin, end / 2,
-                                   end / 2 + 1, end);        
-            }
-
-          }
-
-
-
-
-
-
-
-
-          public static int[] PartialFusion(int[] A, int[] B, int StartA, int EndA, int StartB, int EndB) {
-            int lenA = EndA - StartA + 1,
-                lenB = EndB - StartB + 1,
-                LenResult = lenA + lenB;
-
-            int[] Result = new int[LenResult];
-            int i = StartA,
-                j = StartB,
-                k = 0;
-            while(i < StartA + lenA && j < StartB + lenB) {
-              if(A[i] < B[j]) {
-                Result[k] = A[i];
-                ++i;
-                ++k;
-              }
-              else {
-                Result[k] = B[j];
-                ++j;
-                ++k;
-              }
-            }
-
-            //
-            if(lenA < lenB) {
-              //++j;
-              while(k < LenResult) {
-                Result[k] = B[j];
-                ++j;
-                ++k;
-              }
-            }
-            else {
-              //++i;
-              while(k < LenResult) {
-                Result[k] = A[i];
-                ++i;
-                ++k;
-              }
-            }
-
-
-            return Result;
-          }
-
-
-
-
-
-
-
-
-          public static int[] Fusion(int[]A, int[] B) {
-            int lenA = A.Length,
-                lenB = B.Length,
-                LenResult = lenA + lenB;
-            int[] Result = new int[LenResult];
-            int i = 0,
-                j = 0,
-                k = 0;
-
-            while(i < lenA && j < lenB) {
-              if(A[i] < B[j]) {
-                Result[k] = A[i];
-                ++i;
-                ++k;
-              }
-              else {
-                Result[k] = B[j];
-                ++j;
-                ++k;
-              }
-            }
-
-            //
-            if(lenA < lenB) {
-              //++j;
-              while(k < LenResult) {
-                Result[k] = B[j];
-                ++j;
-                ++k;
-              }
-            }
-            else {
-              //++i;
-              while(k < LenResult) {
-                Result[k] = A[i];
-                ++i;
-                ++k;
-              }
-            }
-
-            return Result;
-          }
-
-          */
 
     }
+
+
+
+
+
+
+
+
+
+    public static int[] FusionSort(int[] tab, int begin, int end) {
+      if(tab.Length <= 1) {
+        return tab;
+      }
+      else {
+
+        return PartialFusion(FusionSort(tab, begin, (begin + end) / 2),
+                             FusionSort(tab, (begin + end) / 2 + 1, end), begin, (begin + end) / 2, (begin + end) / 2 + 1, end);
+      }
+
+    }
+
+
+
+
+
+
+
+
+    public static int[] PartialFusion(int[] A, int[] B, int StartA, int EndA, int StartB, int EndB) {
+      int lenA = EndA - StartA + 1,
+          lenB = EndB - StartB + 1,
+          LenResult = lenA + lenB;
+
+      int[] Result = new int[LenResult];
+      int i = StartA,
+          j = StartB,
+          k = 0;
+      while(i < StartA + lenA && j < StartB + lenB) {
+        if(A[i] < B[j]) {
+          Result[k] = A[i];
+          ++i;
+          ++k;
+        }
+        else {
+          Result[k] = B[j];
+          ++j;
+          ++k;
+        }
+      }
+
+      //
+      if(lenA < lenB) {
+        //++j;
+        while(k < LenResult) {
+          Result[k] = B[j];
+          ++j;
+          ++k;
+        }
+      }
+      else {
+        //++i;
+        while(k < LenResult) {
+          Result[k] = A[i];
+          ++i;
+          ++k;
+        }
+      }
+
+
+      return Result;
+    }
+
+
+
+
+
+
+
+
+    public static int[] Fusion(int[] A, int[] B) {
+      int lenA = A.Length,
+          lenB = B.Length,
+          LenResult = lenA + lenB;
+      int[] Result = new int[LenResult];
+      int i = 0,
+          j = 0,
+          k = 0;
+
+      while(i < lenA && j < lenB) {
+        if(A[i] < B[j]) {
+          Result[k] = A[i];
+          ++i;
+          ++k;
+        }
+        else {
+          Result[k] = B[j];
+          ++j;
+          ++k;
+        }
+      }
+
+      //
+      if(lenA < lenB) {
+        //++j;
+        while(k < LenResult) {
+          Result[k] = B[j];
+          ++j;
+          ++k;
+        }
+      }
+      else {
+        //++i;
+        while(k < LenResult) {
+          Result[k] = A[i];
+          ++i;
+          ++k;
+        }
+      }
+
+      return Result;
+    }
+  
+
+         
+
+    
   }
 }
