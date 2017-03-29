56f6b8f2e1e10dc2359c5d2c18ab1aa69143e1f5
MohamedSellahi <mohamed.sellahi@gmail.com>
2017-03-29 23:48:40 +0200

    Interfaces

---
+++ CSharpBook/Interfaces00/Interfaces00/MyAbstractClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Interfaces00 {
+  abstract class MyAbstractClass {
+
+    protected int _Member1;
+    private int _ParentOnly;
+    public int ParantOnly {
+      get { return _ParentOnly; }
+         set { _ParentOnly = value; } }
+
+    public MyAbstractClass() {
+    }
+
+    public abstract void DisplayMessage();
+    public void MessageFromParent() {
+      Console.WriteLine("Message from the parent");
+    }
+
+    
+
+
+  }
+}
---
+++ CSharpBook/Interfaces00/Interfaces00/MyClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces00 {
+
+  internal class MyClass:MyAbstractClass {
+    public MyClass(){
+    }
+
+    public override void DisplayMessage() {
+      Console.WriteLine("Message from Derived class "); ;
+    }
+
+
+  }
+}
