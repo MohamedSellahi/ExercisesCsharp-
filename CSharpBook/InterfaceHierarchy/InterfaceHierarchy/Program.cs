0ee16afe296ff97297c85dd5f2364077040ebfa2
MohamedSellahi <mohamed.sellahi@gmail.com>
2017-03-30 22:21:06 +0200

    MIInterfacesDone

---
+++ CSharpBook/InterfaceHierarchy/InterfaceHierarchy/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceHierarchy {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("***** Simple Interface Hierarchy *****");
+      // Call from object level
+      BitmapImage myBitmap = new BitmapImage();
+      myBitmap.Draw();
+      myBitmap.DrawInBoundinBox(10,10,20,20);
+      myBitmap.DrawUpsideDown();
+
+      // Get IAdvancedDraw explicitly
+      IAdvancedDraw iADraw = myBitmap as IAdvancedDraw;
+      if (iADraw != null)
+        iADraw.DrawUpsideDown();
+
+
+    }
+  }
+}
---
+++ CSharpBook/InterfaceNameClash/InterfaceNameClash/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceNameClash {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("**** Fun With Interface Name Clashes *****");
+      Octagon oct = new Octagon();
+
+      IDrawToForm itfForm = (IDrawToForm)oct;
+      IDrawToMemory itfMemory = (IDrawToMemory)oct;
+      IDrawToPrinter itfPrinter = (IDrawToPrinter)oct;
+      ISave itfSave = (ISave)oct;
+
+      oct.Draw();
+      itfForm.Draw();
+      itfMemory.Draw();
+      itfPrinter.Draw();
+      itfSave.Save();
+
+      // we can use shorthand cast to access explicitly implemented interfaces
+      ((IDrawToForm)oct).Draw();
+      
+
+      
+
+
+    }
+  }
+}
